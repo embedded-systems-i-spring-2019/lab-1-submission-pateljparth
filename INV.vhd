e5d46301a397c7659aa853e03b77d7a32783d1c2
pateljparth <43219778+pateljparth@users.noreply.github.com>
2019-02-28 13:47:36 -0500

    Add files via upload

---
+++ INV.vhd
@@ -0,0 +1,44 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/27/2019 09:31:41 PM
+-- Design Name: 
+-- Module Name: INV - INV
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity INV is
+    Port ( IO : in Q;
+           O : out STD_LOGIC);
+end INV;
+
+architecture INV of INV is
+
+begin
+
+
+end INV;
---
+++ parallelload.vhd
@@ -0,0 +1,72 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 02/27/2019 09:46:39 PM
+-- Design Name: 
+-- Module Name: parallelload - parallelload
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+
+use IEEE.STD_LOGIC_1164.ALL;
+
+use IEEE.STD_LOGIC_ARITH.ALL;
+
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity updown is
+
+    Port ( parallelload : in  STD_LOGIC_VECTOR (3 downto 0);
+
+           clk,load,dir : in  STD_LOGIC;
+
+           reset : in  STD_LOGIC;
+
+           count : out  STD_LOGIC_VECTOR (3 downto 0));
+
+end updown;
+
+ 
+
+architecture Behavioral of updown is
+
+signal temp:std_logic_vector(3 downto 0);
+
+begin
+
+process(clk,reset)
+
+begin
+
+if reset='1' then temp<="0000";
+
+elsif(clk='1' and clk'event) then
+
+if load='1' then temp<=parallelload;
+
+elsif(load='0' and dir='0') then temp<=temp+1;
+
+elsif(load='0' and dir='1') then temp<=temp-1;
+
+end if;
+
+end if;
+
+end process;
+
+count<=temp;
+
+end Behavioral;
